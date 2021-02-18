d9a73c00de1b15db28b00d7fc410ab2294d2864a
Kaustubh Iyer <kaustubhiyer1998@gmail.com>
2021-02-19 01:45:25 +0530

    more express- routing

--- express/app.js
+++ express/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log("I'll always be here.");
-    res.send('<h1>Henlo</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
